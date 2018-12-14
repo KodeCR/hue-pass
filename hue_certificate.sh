1277dad0afad620dbc946665112c935ec0e12797
KodeCR <KodeCR@users.noreply.github.com>
2018-12-14 00:40:27 +0000

    Change certificate expiration to 11 years

--- hue_certificate.sh
+++ hue_certificate.sh
@@ -2,7 +2,7 @@
 if [ ! -f 'hue_certificate.pem' ]; then
 	bridgeid=$1
 	dec_bridgeid=`python3 -c "print(int(\"$bridgeid\", 16))"`
-	openssl req -new  -config hue_openssl.conf  -nodes -x509 -newkey  ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve   -subj "/C=NL/O=Philips Hue/CN=$bridgeid" -keyout hue_private.key -out hue_public.crt -set_serial $dec_bridgeid
+	openssl req -new -config hue_openssl.conf -nodes -x509 -days 4017 -newkey ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve -subj "/C=NL/O=Philips Hue/CN=$bridgeid" -keyout hue_private.key -out hue_public.crt -set_serial $dec_bridgeid
 	if [ $? -ne 0 ] ; then
 		echo -e "\033[31m ERROR!! Local certificate generation failed!\033[0m"
 	else
